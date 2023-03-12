3bef74d5d57f2b4b9b5b52237741fa52f6974606
mabasiri95 <111532317+mabasiri95@users.noreply.github.com>
2023-03-12 17:09:38 -0500

    Update SIndy.py

--- SIndy.py
+++ SIndy.py
@@ -6,3 +6,10 @@ Created on Sun Mar 12 16:55:52 2023
 @author: mohammadaminbasiri
 """
 
+import numpy as np
+import pysindy as ps
+
+t = np.linspace(0, 1, 100)
+x = 3 * np.exp(-2 * t)
+y = 0.5 * np.exp(t)
+X = np.stack((x, y), axis=-1) 
